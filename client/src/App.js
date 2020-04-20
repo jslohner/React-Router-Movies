1095527dcd9ba729150efdb3605ee3fb26d88ffe
Justin Lohner <lohner4@gmail.com>
2020-04-20 14:00:12 -0600

    Added movie and movie list in the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
 	const [savedList, setSavedList] = useState([]);
@@ -9,10 +13,24 @@ const App = () => {
 		setSavedList([...savedList, movie]);
 	};
 
+	console.log(savedList);
+
+	if(!savedList) {
+		return <h2>Data Loading</h2>
+	}
+
 	return (
 		<div>
-			<SavedList list={savedList} />
-			<div>Replace this Div with your Routes</div>
+			<Switch>
+				<Route path='/movies/:id'>
+					<Movie />
+				</Route>
+
+				<Route path='/'>
+					<SavedList list={savedList} />
+					<MovieList />
+				</Route>
+			</Switch>
 		</div>
 	);
 };
