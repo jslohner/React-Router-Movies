03455e2158a884d37f9f54e4615f3b8dcd4c0ee0
Justin Lohner <lohner4@gmail.com>
2020-04-20 13:04:59 -0600

    Wrapped app in router

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import React, { useState } from 'react';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<div>Replace this Div with your Routes</div>
+		</div>
+	);
 };
 
 export default App;
